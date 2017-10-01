538073149646bf57520a1b7a54a4954d7d523d00
Adnard-kod <andra.lally@gmail.com>
2017-10-01 13:49:58 +0200

    fixed recompose errors

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,16 +1,9 @@
 import React from "react";
-import styles from "./TestStyles";
-import { COLORS } from "../../styles/constants/colors.js";
 
-const Test = () => {
-  return (
-    <div>
-      <h1 className={styles.textColor(COLORS.yellow)}>Test</h1>
-      <h1 className={styles.textColor(COLORS.blue)}>Test</h1>
-      <h1 className={styles.textColor(COLORS.dark)}>Test</h1>
-      <h1 className={styles.textColor(COLORS.orangeDark)}>Test</h1>
-    </div>
-  )
-};
+const App = () => (
+  <div className="App">
+    hey
+  </div>
+);
 
-export default Test;
+export default App;
